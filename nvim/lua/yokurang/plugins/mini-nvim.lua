466dd9a12fba94a1de3a5bde2ab3cb40e014ba78
You Guang <yokurang@gmail.com>
2024-01-17 12:01:27 +0800

    neovim mini

--- nvim/lua/yokurang/plugins/mini-nvim.lua
+++ nvim/lua/yokurang/plugins/mini-nvim.lua
@@ -1,1 +1,27 @@
-return { "echasnovski/mini.nvim", version = "*" }
+return {
+	"echasnovski/mini.nvim",
+	version = "*",
+	config = function()
+		require("mini.animate").setup()
+		require("mini.ai").setup()
+		require("mini.align").setup()
+		require("mini.comment").setup()
+		require("mini.cursorword").setup()
+		require("mini.completion").setup()
+		require("mini.doc").setup()
+		require("mini.indentscope").setup()
+		local hipatterns = require("mini.hipatterns")
+		hipatterns.setup({
+			highlighters = {
+				-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+				fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+				hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+				-- Highlight hex color strings (`#rrggbb`) using that color
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
+	end,
+}
