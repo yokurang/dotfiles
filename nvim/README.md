abc432b0900d50735be42745a14859c6113b6f60
You Guang <yokurang@gmail.com>
2024-08-11 23:22:02 +0800

    updated kitty dotfiles

--- nvim/README.md
+++
@@ -1,3 +0,0 @@
-# My Neovim Configurations 
-
-This repository contains a copy of my neovim configurations as I learn more about this editor and its plugins.
--- nvim/lua/yokurang/plugins/markdown-preview.lua
+++ nvim/lua/yokurang/plugins/markdown-preview.lua
@@ -1,17 +1,17 @@
 return {
+  -- Install markdown preview, use npx if available.
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
-  config = function()
-  vim.g.mkdp_auto_start = 1
-  vim.g.mkdp_auto_close = 1
-  vim.g.mkdp_refresh_slow = 1
-  vim.g.mkdp_open_to_the_world = 1
-
-  local keymap = vim.keymap
-  keymap.set('n', '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>')
-  keymap.set('n', '<leader>ms', '<cmd>MarkdownPreviewStop<cr>')
+  build = function(plugin)
+    if vim.fn.executable "npx" then
+      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+    else
+      vim.cmd [[Lazy load markdown-preview.nvim]]
+      vim.fn["mkdp#util#install"]()
+    end
+  end,
+  init = function()
+    if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
   end,
 }
-
